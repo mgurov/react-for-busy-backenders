e68a44a1b4c6ab33571788434bcbffbe14430251
Mykola Gurov <ngurov@gmail.com>
2021-01-20 07:27:01 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 const data = {
   "page": 1,
   "per_page": 6,
@@ -58,12 +65,73 @@ const data = {
 function App() {
   return (
     <div className="App">
-      <Json value={data}/>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
+      <Json value={data} />
     </div>
   );
 }
 
-function Json({value}) {
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function Json({ value }) {
   return <pre>
     {JSON.stringify(value, null, 2)}
   </pre>
