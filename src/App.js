deddf2f8eb4fd941fcc2f94bd20069eb3fdb864b
Mykola Gurov <ngurov@gmail.com>
2021-01-20 20:58:26 +0100

    router added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 
 import Json from './components/Json';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
 
   return (
@@ -19,10 +26,54 @@ function App() {
         </a>
       </header>
 
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
     <div>this is the json: <Json value={theDataExample}/></div>
 
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 const theDataExample = {
