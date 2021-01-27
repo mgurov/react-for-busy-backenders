1be5e1cd087d74893177ab6d553029587c00ab8f
Mykola Gurov <ngurov@gmail.com>
2021-01-27 16:49:14 +0100

    json done

--- src/components/Json.js
+++ src/components/Json.js
@@ -6,14 +6,17 @@ class Json extends React.Component {
     constructor(props) {
         super(props);
         this.state = {open: false}
+        this.toggleState = () => {
+            this.setState({open: !this.state.open})
+        }
     }
 
     render() {
         if (this.state.open) {
-            return <pre><code>{JSON.stringify(this.props.value, null, 2)}</code></pre>;
+            return <pre onClick={this.toggleState}><code>{JSON.stringify(this.props.value, null, 2)}</code></pre>;
         } else {
             return <>
-            <Button size="sm">json</Button>
+            <Button size="sm" variant="light" onClick={this.toggleState}>json</Button>
             </>
         }
         
