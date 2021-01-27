79ea2679dbe06871323235cfc1d050841936aa90
Mykola Gurov <ngurov@gmail.com>
2021-01-27 16:58:34 +0100

    list is done

--- src/App.js
+++ src/App.js
@@ -36,28 +36,19 @@ function ListUsers({users}) {
       <tr>
         <th>#</th>
         <th>First Name</th>
-        <th>Last Name</th>
-        <th>Username</th>
+        <th>json</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>1</td>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td colSpan="2">Larry the Bird</td>
-        <td>@twitter</td>
-      </tr>
+      {
+        users.map(user => {
+        return <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.first_name}</td>
+          <td><Json value={user} /></td>
+        </tr>
+        })
+      }
     </tbody>
     </Table>
     <Json value={users}/>
