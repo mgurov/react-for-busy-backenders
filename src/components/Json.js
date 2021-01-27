26c649faf8662d1f4f295804537b677d788e2de1
Mykola Gurov <ngurov@gmail.com>
2021-01-27 16:44:23 +0100

    button className

--- src/components/Json.js
+++ src/components/Json.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 
 class Json extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {open: false}
+    }
+
     render() {
-        return <pre><code>{JSON.stringify(this.props.value, null, 2)}</code></pre>;
+        if (this.state.open) {
+            return <pre><code>{JSON.stringify(this.props.value, null, 2)}</code></pre>;
+        } else {
+            return <button className="btn primary">json</button>
+        }
+        
     }
 }
 
