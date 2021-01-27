0834a1b4f5e99f3d6035e6943722110e2466fd13
Mykola Gurov <ngurov@gmail.com>
2021-01-27 16:54:32 +0100

    the table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Json from './components/Json'
+import {Table} from 'react-bootstrap'
 
 function App() {
   return (
@@ -29,7 +30,38 @@ function App() {
 }
 
 function ListUsers({users}) {
-  return <Json value={users}/>
+  return <>
+    <Table striped bordered hover>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Username</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td colSpan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+    </Table>
+    <Json value={users}/>
+  </>
 }
 
 const theData = {
