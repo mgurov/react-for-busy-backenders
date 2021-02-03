0124dd7f7c24c959181f1019029a9f8dbd89f2fd
Mykola Gurov <ngurov@gmail.com>
2021-02-03 17:45:12 +0100

    lodash

--- src/components/OrdersFetcher.js
+++ src/components/OrdersFetcher.js
@@ -5,6 +5,7 @@ import {
     Link,
   } from "react-router-dom";
 import Fetcher from './Fetcher'
+import _ from 'lodash'
 
 function OrderFun({linkUrl}) {
     return <Fetcher url="/api/orders/?limit=1000" onData={data => <OrdersList linkUrl={linkUrl} data={data} />} />
@@ -17,7 +18,7 @@ function OrdersList({ linkUrl, data:orders }) {
         <thead>
           <tr>
             <th>#</th>
-            <th>First Name</th>
+            <th>Lines</th>
             <th>json</th>
           </tr>
         </thead>
@@ -26,7 +27,7 @@ function OrdersList({ linkUrl, data:orders }) {
             orders.map(order => {
               return <tr key={order.id}>
                 <td><Link to={`${linkUrl}/id/${order.id}`}>{order.id}</Link></td>
-                <td>{order.first_name}</td>
+                <td>{_.size(order.lines)} ( {_.sumBy(order.lines, 'price')} )</td>
                 <td><Json value={order} /></td>
               </tr>
             })
