682978223ff5b986283cb912fde33a8d1db80d69
Mykola Gurov <ngurov@gmail.com>
2021-02-03 16:47:40 +0100

    extracted fetcher

---
+++ src/components/Fetcher.js
@@ -0,0 +1,49 @@
+import React from 'react';  
+
+class Fetcher extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            data: []
+        };
+    }
+
+    componentDidMount() {
+        fetch(this.props.url)
+            .then(res => res.json())
+            //TODO: status should be 200
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        data: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, data } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return this.props.onData(data);
+        }
+    }
+}
+
+
+export default Fetcher;
