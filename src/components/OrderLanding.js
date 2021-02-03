1432700fdc194e9cc455e6361ea5f8cb4fcd9652
Mykola Gurov <ngurov@gmail.com>
2021-02-03 17:37:29 +0100

    noice

--- src/components/OrderLanding.js
+++ src/components/OrderLanding.js
@@ -2,39 +2,48 @@ import React from 'react'
 
 import Fetcher from './Fetcher'
 import Json from './Json'
-import {Card} from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 
 import {
     useParams,
 } from "react-router-dom";
-  
+
 
 function Order() {
     let { orderId } = useParams();
-    return <Fetcher 
-        url={`/api/orders/id/${orderId}`} 
-        onData={order => <RenderOrder order={order} />} 
+    return <Fetcher
+        url={`/api/orders/id/${orderId}`}
+        onData={order => <RenderOrder order={order} />}
     />
 }
 
-function RenderOrder({order}) {
+function RenderOrder({ order }) {
     return <>
 
 
-        <Card style={{ width: '18rem' }}>
-        <Card.Body>
-            <Card.Title>Id</Card.Title>
-            <Card.Text>
-            {order.id}
-            </Card.Text>
-        </Card.Body>
+        <Card>
+            <Card.Body>
+                <Card.Title>Id</Card.Title>
+                <Card.Text>
+                    {order.id}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+
+        <Card>
+            <Card.Body>
+                <Card.Title>Customer</Card.Title>
+                <Card.Text>
+                    {order.customerId} <Fetcher
+                        url={`/api/customers/id/${order.customerId}`}
+                        onData={customer => <Json value={customer} />}
+                    />
+                </Card.Text>
+            </Card.Body>
         </Card>
 
         <Json value={order} open={true} />
-        <Fetcher 
-        url={`/api/customers/id/${order.customerId}`} 
-        onData={customer => <Json value={customer} />} 
-        />
+
     </>
 }
 
