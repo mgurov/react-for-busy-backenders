f146cc1a04d98f95d4f2bbd3f2b0b304c3391d8a
Mykola Gurov <ngurov@gmail.com>
2021-02-03 16:15:21 +0100

    export default OrderFetcher

---
+++ src/components/OrdersFetcher.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Json from './Json';
+
+class OrderFetcher extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            data: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("/api/orders/?limit=2")
+            .then(res => res.json())
+            //TODO: status should be 200
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        data: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, data } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <Json value={data} />
+            );
+        }
+    }
+}
+
+export default OrderFetcher;
